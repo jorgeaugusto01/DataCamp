7d3e6a0dbffabf524d93f1cc88e2466b7f63c0c7
jorge.araujo <jorge.araujo@trtrio.gov.br>
2018-08-01 11:46:35 -0300

    1

--- Supervised_Learning/Cap_3/Pratice_5.py
+++ Supervised_Learning/Cap_3/Pratice_5.py
@@ -1,8 +1,12 @@
 # Import necessary modules
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
+from scipy.stats import randint
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import RandomizedSearchCV
 import numpy as np
 import pandas as pd
+from sklearn.metrics import confusion_matrix, classification_report
 
 
 seeds = pd.read_csv('../../DataSets/seeds/seeds.csv')
@@ -33,4 +37,21 @@ logreg_cv.fit(X, y)
 print("Tuned Logistic Regression Parameters: {}".format(logreg_cv.best_params_))
 print("Best score is {}".format(logreg_cv.best_score_))
 
+# Setup the parameters and distributions to sample from: param_dist
+param_dist = {"max_depth": [3, None],
+              "max_features": randint(1, 9),
+              "min_samples_leaf": randint(1, 9),
+              "criterion": ["gini", "entropy"]}
 
+# Instantiate a Decision Tree classifier: tree
+tree = DecisionTreeClassifier()
+
+# Instantiate the RandomizedSearchCV object: tree_cv
+tree_cv = RandomizedSearchCV(tree, param_dist, cv=5)
+
+# Fit it to the data
+tree_cv.fit(X, y)
+
+# Print the tuned parameters and score
+print("Tuned Decision Tree Parameters: {}".format(tree_cv.best_params_))
+print("Best score is {}".format(tree_cv.best_score_))
