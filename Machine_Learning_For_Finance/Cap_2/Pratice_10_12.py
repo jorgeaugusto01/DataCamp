76c3f419ef5d0ebaad700a16bfacf651b22e9399
jorgeaugusto01 <jorgeaugusto01@gmail.com>
2018-08-21 01:24:45 -0300

    1

--- Machine_Learning_For_Finance/Cap_2/Pratice_10_12.py
+++ Machine_Learning_For_Finance/Cap_2/Pratice_10_12.py
@@ -308,5 +308,140 @@ plt.scatter(train_predictions, train_targets, label='train')
 plt.scatter(test_predictions, test_targets, label='test')
 plt.show()
 
+#Fit a random forest
+#Data scientists often use random forest models. They perform well out of the box, and have lots of settings to optimize performance.
+# Random forests can be used for classification or regression; we'll use it for regression to predict the future price change of LNG.
+#we'll create and fit the random forest model similarly to the decision trees using the .fit(features, targets) method.
+# With sklearn's RandomForestRegressor, there's a built-in .score() method we can use to evaluate performance.
+# This takes arguments (features, targets), and returns the R2 score (the coefficient of determination).
 
+from sklearn.ensemble import RandomForestRegressor
+
+# Create the random forest model and fit to the training data
+rfr = RandomForestRegressor(n_estimators=200)
+rfr.fit(train_features, train_targets)
+
+# Look at the R^2 scores on train and test
+print("-----------------------------------")
+print(rfr.score(train_features, train_targets))
+print(rfr.score(test_features, test_targets))
+
+#Tune random forest hyperparameters
+#As with all models, we want to optimize performance by tuning hyperparameters. We have many hyperparameters
+# for random forests, but the most important is often the number of features we sample at each split,
+# or max_features in RandomForestRegressor from the sklearn library. For models like random forests that have
+# randomness built-in, we also want to set the random_state. This is set for our results to be reproducible.
+#Usually, we can use sklearn's GridSearchCV() method to search hyperparameters, but with a financial time series,
+# we don't want to do cross-validation due to data mixing. We want to fit our models on the oldest data and evaluate
+# on the newest data. So we'll use sklearn's ParameterGrid to create combinations of hyperparameters to search.
+
+from sklearn.model_selection import ParameterGrid
+
+# Create a dictionary of hyperparameters to search
+grid = {'n_estimators': [200], 'max_depth': [3], 'max_features': [4, 8], 'random_state': [42]}
+test_scores = []
+
+# Loop through the parameter grid, set the hyperparameters, and save the scores
+for g in ParameterGrid(grid):
+    rfr.set_params(**g)  # ** is "unpacking" the dictionary
+    rfr.fit(train_features, train_targets)
+    test_scores.append(rfr.score(test_features, test_targets))
+
+# Find best hyperparameters from the test score and print
+best_idx = np.argmax(test_scores)
+print("-----------------------------------")
+print(test_scores[best_idx], ParameterGrid(grid)[best_idx])
+
+
+#Evaluate performance
+#Lastly, and as always, we want to evaluate performance of our best model to check how
+# well or poorly we are doing. Ideally it's best to do back-testing, but that's an involved
+# process we don't have room to cover in this course.#
+# We've already seen the R2 scores, but let's take a look at the scatter plot of predictions vs actual
+# results using matplotlib. Perfect predictions would be a diagonal line from the lower left to the upper right.
+
+# Use the best hyperparameters from before to fit a random forest model
+rfr = RandomForestRegressor(n_estimators=200, max_depth=3, max_features=4, random_state=42)
+rfr.fit(train_features, train_targets)
+
+# Make predictions with our model
+train_predictions = rfr.predict(train_features)
+test_predictions = rfr.predict(test_features)
+
+# Create a scatter plot with train and test actual vs predictions
+plt.scatter(train_targets, train_predictions, label='train')
+plt.scatter(test_targets, test_predictions, label='test')
+plt.legend()
+plt.show()
+
+#Random forest feature importances
+#One useful aspect of tree-based methods is the ability to extract feature importances.
+# This is a quantitative way to measure how much each feature contributes to our predictions.
+# It can help us focus on our best features, possibly enhancing or tuning them, and can also help
+# us get rid of useless features that may be cluttering up our model.
+#Tree models in sklearn have a .feature_importances_ property that's accessible after fitting the model.
+# This stores the feature importance scores. We need to get the indices of the sorted feature importances using np.argsort()
+# in order to make a nice-looking bar plot of feature importances (sorted from greatest to least importance).
+
+# Get feature importances from our random forest model
+importances = rfr.feature_importances_
+
+# Get the index of importances from greatest importance to least
+sorted_index = np.argsort(importances)[::-1]
+x = range(len(importances))
+
+# Create tick labels
+labels = np.array(feature_names)[sorted_index]
+plt.bar(x, importances[sorted_index], tick_label=labels)
+
+# Rotate tick labels to vertical
+plt.xticks(rotation=90)
+plt.show()
+
+
+#A gradient boosting model
+#Now we'll fit a gradient boosting (GB) model. It's been said a linear model is like a Toyota Camry,
+# and GB is like a Black Hawk helicopter. GB has potential to outperform random forests, but doesn't always do so.
+# This is called the no free lunch theorem, meaning we should always try lots of different models for each problem.
+#GB is similar to random forest models, but the difference is that trees are built successively. With each iteration,
+# the next tree fits the residual errors from the previous tree in order to improve the fit.
+#For now we won't search our hyperparameters -- they've been searched for you.
+
+from sklearn.ensemble import GradientBoostingRegressor
+
+# Create GB model -- hyperparameters have already been searched for you
+gbr = GradientBoostingRegressor(max_features=4,
+                                learning_rate=0.01,
+                                n_estimators=200,
+                                subsample=0.6,
+                                random_state=42)
+gbr.fit(train_features, train_targets)
+
+print(gbr.score(train_features, train_targets))
+print(gbr.score(test_features, test_targets))
+
+
+#Gradient boosting feature importances
+#Just like with random forests, we can extract feature importances from gradient boosting models in
+# order to understand which features seem to be the best predictors. Sometimes it's nice to try different tree-based
+# models (gradient boosting, random forests, decision trees, adaboost, etc) and look at the feature importances from all of them.
+# This can help average out any peculiarities that may arise from one particular model.
+# Once again, the feature importances are stored as a numpy array in the .feature_importances_ property of the
+# gradient boosting model. We'll need to get the sorted indices of the feature importances, using np.argsort(), in order to make a nice plot.
+
+# Extract feature importances from the fitted gradient boosting model
+feature_importances = gbr.feature_importances_
+
+# Get the indices of the largest to smallest feature importances
+sorted_index = np.argsort(feature_importances)[::-1]
+x = range(features.shape[1])
+
+# Create tick labels
+labels = np.array(feature_names)[sorted_index]
+
+plt.bar(x, feature_importances[sorted_index], tick_label=labels)
+
+# Set the tick lables to be the feature names, according to the sorted feature_idx
+plt.xticks(rotation=90)
+plt.show()
 
