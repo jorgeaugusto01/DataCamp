654ed3a913962008e7b14d75c7daa423a01440dc
jorge.araujo <jorge.araujo@trtrio.gov.br>
2019-06-13 17:59:44 -0300

    commit

--- Data Scientist with Python/8_pandas_foundation/2_Exploratory_data_analysis/Exercicio.py
+++ Data Scientist with Python/8_pandas_foundation/2_Exploratory_data_analysis/Exercicio.py
@@ -167,3 +167,77 @@ titanic.loc[titanic['pclass'] == 3].plot(ax=axes[2], y='fare', kind='box')
 
 # Display the plot
 plt.show()
+
+'''
+10
+Reading in a data file
+Now that you have identified the method to use to read the data, let's try to read one file. The problem with real data such as this is that the files are almost never formatted in a convenient way. In this exercise, there are several problems to overcome in reading the file. First, there is no header, and thus the columns don't have labels. There is also no obvious index column, since none of the data columns contain a full date or time.
+Your job is to read the file into a DataFrame using the default arguments. After inspecting it, you will re-read the file specifying that there are no headers supplied.
+The CSV file has been provided for you as the variable data_file.
+'''
+# Import pandas
+import pandas as pd
+
+# Read in the data file: df
+df = pd.read_csv(data_file)
+
+# Print the output of df.head()
+print(df.head())
+
+# Read in the data file with header=None: df_headers
+df_headers = pd.read_csv(data_file, header=None)
+
+# Print the output of df_headers.head()
+print(df_headers.head())
+
+'''
+11
+Re-assigning column names
+After the initial step of reading in the data, the next step is to clean and tidy it so that it is easier to work with.
+In this exercise, you will begin this cleaning process by re-assigning column names and dropping unnecessary columns.
+pandas has been imported in the workspace as pd, and the file NOAA_QCLCD_2011_hourly_13904.txt has been parsed and loaded into a DataFrame df. The comma separated string of column names, column_labels, and list of columns to drop, list_to_drop, have also been loaded for you.
+'''
+# Split on the comma to create a list: column_labels_list
+column_labels_list = column_labels.split(',')
+
+# Assign the new column labels to the DataFrame: df.columns
+df.columns = column_labels_list
+
+# Remove the appropriate columns: df_dropped
+df_dropped = df.drop(list_to_drop, axis='columns')
+
+# Print the output of df_dropped.head()
+print(df_dropped.head())
+
+'''
+12
+Cleaning and tidying datetime data
+In order to use the full power of pandas time series, you must construct a DatetimeIndex. To do so, it is necessary to clean and transform the date and time columns.
+The DataFrame df_dropped you created in the last exercise is provided for you and pandas has been imported as pd.
+Your job is to clean up the date and Time columns and combine them into a datetime collection to be used as the Index.
+'''
+# Convert the date column to string: df_dropped['date']
+df_dropped['date'] = df_dropped['date'].astype(str)
+
+# Pad leading zeros to the Time column: df_dropped['Time']
+df_dropped['Time'] = df_dropped['Time'].apply(lambda x:'{:0>4}'.format(x))
+
+# Concatenate the new date and Time columns: date_string
+date_string = df_dropped['date'] + df_dropped['Time']
+
+# Convert the date_string Series to datetime: date_times
+date_times = pd.to_datetime(date_string, format='%Y%m%d%H%M')
+
+# Set the index to be the new date_times container: df_clean
+df_clean = df_dropped.set_index(date_times)
+
+# Print the output of df_clean.head()
+print(df_clean.head())
+
+'''
+13
+Cleaning the numeric columns
+The numeric columns contain missing values labeled as 'M'. In this exercise, your job is to transform these columns such that they contain only numeric values and interpret missing data as NaN.
+The pandas function pd.to_numeric() is ideal for this purpose: It converts a Series of values to floating-point values. Furthermore, by specifying the keyword argument errors='coerce', you can force strings like 'M' to be interpreted as NaN.
+A DataFrame df_clean is provided for you at the start of the exercise, and as usual, pandas has been imported as pd.
+'''
