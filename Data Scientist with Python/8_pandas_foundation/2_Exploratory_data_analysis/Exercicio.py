4f880e7024672c0e619895b89fcf864379633454
jorge.araujo <jorge.araujo@trtrio.gov.br>
2019-06-13 11:17:44 -0300

    Commit

--- Data Scientist with Python/8_pandas_foundation/2_Exploratory_data_analysis/Exercicio.py
+++ Data Scientist with Python/8_pandas_foundation/2_Exploratory_data_analysis/Exercicio.py
@@ -1,4 +1,6 @@
-#01 - pandas line plots
+'''
+01 - pandas line plots
+'''
 # Create a list of y-axis column names: y_columns
 y_columns = ['AAPL', 'IBM']
 
@@ -14,7 +16,9 @@ plt.ylabel('Price ($US)')
 # Display the plot
 plt.show()
 
-#02 - pandas scatter plots
+'''
+02 - pandas scatter plots
+'''
 # Generate a scatter plot
 df.plot(kind='scatter', x='hp', y='mpg', s=sizes)
 
@@ -30,7 +34,9 @@ plt.ylabel('Fuel efficiency (mpg)')
 # Display the plot
 plt.show()
 
-#03 - pandas box plots
+'''
+03 - pandas box plots
+'''
 # Make a list of the column names to be plotted: cols
 cols = ['weight', 'mpg']
 
@@ -40,7 +46,10 @@ df[cols].plot(subplots=True, kind='box')
 # Display the plot
 plt.show()
 
-#04 pandas hist, pdf and cdf
+
+'''
+04 pandas hist, pdf and cdf
+'''
 # This formats the plots such that they appear on separate rows
 fig, axes = plt.subplots(nrows=2, ncols=1)
 
@@ -52,7 +61,10 @@ plt.show()
 df.fraction.plot(ax=axes[1], kind='hist', bins=30, normed=True, cumulative=True, range=(0,.3))
 plt.show()
 
-#05 - Bachelor's degrees awarded to women
+
+'''
+05 - Bachelor's degrees awarded to women
+'''
 # Print the minimum value of the Engineering column
 print(df['Engineering'].min())
 
@@ -69,12 +81,89 @@ mean.plot()
 plt.show()
 
 
-#06 - Median vs mean
+'''
+06 - Median vs mean
 # Print summary statistics of the fare column with .describe()
+'''
 print(df.fare.describe())
 
 # Generate a box plot of the fare column
 df.fare.plot(kind='box')
 
 # Show the plot
+plt.show()
+
+# Quantiles
+# Print the number of countries reported in 2015
+print(df['2015'].count())
+
+# Print the 5th and 95th percentiles
+print(df.quantile([0.05, 0.95]))
+
+# Generate a box plot
+years = ['1800','1850','1900', '1930', '1950','2000']
+df[years].plot(kind='box')
+plt.show()
+
+
+'''
+07 - Standard deviation of temperature
+Let's use the mean and standard deviation to explore differences in temperature distributions in Pittsburgh in 2013. The data has been obtained from Weather Underground.
+In this exercise, you're going to compare the distribution of daily temperatures in January and March. You'll compute the mean and standard deviation for these two months. You will notice that while the mean values are similar, the standard deviations are quite different, meaning that one month had a larger fluctuation in temperature than the other.
+The DataFrames have been pre-loaded for you as january, which contains the January data, and march, which contains the March data.
+'''
+# Print the mean of the January and March data
+print(january.mean(), march.mean())
+
+# Print the standard deviation of the January and March data
+print(january.std(), march.std())
+
+
+'''
+08 - Separate and summarize
+Let's use population filtering to determine how the automobiles in the US differ from the global average and standard deviation. How does the distribution of fuel efficiency (MPG) for the US differ from the global average and standard deviation?
+In this exercise, you'll compute the means and standard deviations of all columns in the full automobile dataset. Next, you'll compute the same quantities for just the US population and subtract the global values from the US values.
+All necessary modules have been imported and the DataFrame has been pre-loaded as df.
+'''
+# Compute the global mean and global standard deviation: global_mean, global_std
+global_mean = df.mean()
+global_std = df.std()
+
+# Filter the US population from the origin column: us
+us = df[df['origin'] == 'US']
+
+# Compute the US mean and US standard deviation: us_mean, us_std
+us_mean = us.mean()
+us_std = us.std()
+
+# Print the differences
+print(us_mean - global_mean)
+print(us_std - global_std)
+
+'''
+09 - Separate and plot
+Population filtering can be used alongside plotting to quickly determine differences in distributions between the sub-populations. You'll work with the Titanic dataset.
+
+There were three passenger classes on the Titanic, and passengers in each class paid a different fare price. In this exercise, you'll investigate the differences in these fare prices.
+
+Your job is to use Boolean filtering and generate box plots of the fare prices for each of the three passenger classes. The fare prices are contained in the 'fare' column and passenger class information is contained in the 'pclass' column.
+
+When you're done, notice the portions of the box plots that differ and those that are similar.
+
+The DataFrame has been pre-loaded for you as titanic
+'''
+
+# Display the box plots on 3 separate rows and 1 column
+fig, axes = plt.subplots(nrows=3, ncols=1)
+
+# Generate a box plot of the fare prices for the First passenger class
+titanic.loc[titanic['pclass'] == 1].plot(ax=axes[0], y='fare', kind='box')
+
+# Generate a box plot of the fare prices for the Second passenger class
+titanic.loc[titanic['pclass'] == 2].plot(ax=axes[1], y='fare', kind='box')
+
+# Generate a box plot of the fare prices for the Third passenger class
+titanic.loc[titanic['pclass'] == 3].plot(ax=axes[2], y='fare', kind='box')
+
+# Display the plot
 plt.show()
