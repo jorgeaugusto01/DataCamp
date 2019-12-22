8068153bc69444d2228b6f47b6b71bd41e06b057
jorgeaugusto01 <jorgeaugusto01@gmail.com>
2019-12-22 18:05:58 -0300

    commit

--- Data Scientist with Python/23_Unsupervised_Learning/Cap_2/pratice2.py
+++ Data Scientist with Python/23_Unsupervised_Learning/Cap_2/pratice2.py
@@ -8,11 +8,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import normalize
 
-seeds = pd.read_csv('DataCamp\DataSets\seeds\\seeds.csv')
-varietisSeeds = pd.read_csv('DataCamp\DataSets\seeds\\varietiesSeeds.csv')
+seeds = pd.read_csv('DataCamp/DataSets/seeds/seeds.csv')
+varietisSeeds = pd.read_csv('DataCamp/DataSets/seeds/varietiesSeeds.csv')
 
-varietisSeedsNumbers = pd.read_csv('DataCamp\DataSets\seeds\\varietiesSeedsNumbers.csv')
-stockMovements = pd.read_csv('portfolio_equity_curve.csv').iloc[2:]
+varietisSeedsNumbers = pd.read_csv('DataCamp/DataSets/seeds/varietiesSeedsNumbers.csv')
+stockMovements = pd.read_csv('Financeiro _beta_corr_cias.csv').iloc[2:]
 #stockMovements = pd.read_csv('precos_normalizados.csv').iloc[2:]
 #stockMovements = pd.read_csv('sumario_15.csv').iloc[2:].set_index(['cod_neg']).join(pd.read_csv('sumario_30.csv').iloc[2:].set_index(['cod_neg']), rsuffix='30').join(pd.read_csv('sumario_60.csv').iloc[2:].set_index(['cod_neg']), rsuffix='60').join(pd.read_csv('sumario_120.csv').iloc[2:].set_index(['cod_neg']), rsuffix='120').join(pd.read_csv('sumario_365.csv').iloc[2:].set_index(['cod_neg']), rsuffix='365')
 stockMovements = stockMovements.reset_index()
